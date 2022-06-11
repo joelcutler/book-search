68f3e8e2bbc7b2626e269ddac8fb776a9862b262
Joel C <joel7921111@yahoo.com>
2022-06-11 00:58:14 -0600

    insstructions through creating front-end files, only did mutations.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+// create our typeDefs
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: String!): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  input BookInput {
+    bookId: String
+    authors : String[]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Book {
+    bookId: String
+    authors : String[]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
